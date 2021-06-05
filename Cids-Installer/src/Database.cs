e04dbabc76e50582e19b994d4e384f3096570a22
HUGHNew <1728902978@qq.com>
2021-06-05 10:22:00 +0800

    [:tada:] init now

---
+++ Cids-Installer/src/Database.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cids_Installer
+{
+    class Database
+    {
+    }
+}
---
+++ Cids-Installer/src/Local.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cids_Installer
+{
+    class Local
+    {
+        public const string MainEnv = "Cids";
+        public const string Id = "CidsUUID";
+        // ProgramFiles
+        public const string PF = "ProgramFiles";
+        public const EnvironmentVariableTarget Process = EnvironmentVariableTarget.Process;
+        public const EnvironmentVariableTarget Machine = EnvironmentVariableTarget.Machine;
+        
+        //todo
+        //set the json
+        public String UuId { get; set; }
+        static Local()
+        {
+            Environment.SetEnvironmentVariable(MainEnv,
+                Environment.GetEnvironmentVariable(PF,Process));
+        }
+    }
+}
