e125f0e022319e25bd79de06bb07956408f8003c
HUGHNew <1728902978@qq.com>
2021-06-06 01:56:42 +0800

    [:wheelchair:] goes well as expected now

--- Cids-Installer/src/Database.cs
+++ Cids-Installer/src/Database.cs
@@ -6,5 +6,45 @@ namespace Cids_Installer
 {
     class Database
     {
+        #region Id Region
+        public const int IdLen= 7;
+        /**
+         * @brief Get the UUID from database
+         */
+        public static string GetId=
+            IDCompletion(GetIdFromDatabase());
+        /**
+         * @brief ID Completion from Int
+         * @warning Assume that id is less than 1000 0000
+         */
+        private static string IDCompletion(int id)
+        {
+            string raw = id.ToString();
+            string prefix = "";
+            if (raw.Length != IdLen)
+            {
+                 prefix = new string('0',IdLen-raw.Length);
+            }
+            return prefix + raw;
+        }
+        /**
+         * @brief Get ID from DataBase as Int indeed
+         */
+        private static int GetIdFromDatabase()
+        {
+            return 0;
+        }
+        #endregion
+
+        // Need to Access SQL Database
+        // something to store data
+        public Database() { }
+        public Database Fetch() {
+            return this;
+        }
+
+        // query
+
+        // append
     }
 }
--- Cids-Installer/src/Local.cs
+++ Cids-Installer/src/Local.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Text;
+using System.Text.Json;
 
 namespace Cids_Installer
 {
@@ -8,18 +9,25 @@ namespace Cids_Installer
     {
         public const string MainEnv = "Cids";
         public const string Id = "CidsUUID";
-        // ProgramFiles
-        public const string PF = "ProgramFiles";
+        public const string PF = "ProgramFiles"; // ProgramFiles
+        public static readonly string ProgramFilesPath = Environment.GetEnvironmentVariable(PF, Process);
         public const EnvironmentVariableTarget Process = EnvironmentVariableTarget.Process;
         public const EnvironmentVariableTarget Machine = EnvironmentVariableTarget.Machine;
-        
+
         //todo
         //set the json
-        public String UuId { get; set; }
+        public static String UuId = Database.GetId;
         static Local()
         {
+            #region Set Env Var
             Environment.SetEnvironmentVariable(MainEnv,
-                Environment.GetEnvironmentVariable(PF,Process));
+                Path.Combine(ProgramFilesPath,MainEnv),Machine); // Cids
+            Environment.SetEnvironmentVariable(Id, UuId, Machine); // CidsUUID
+            #endregion
+        }
+        public static void SaveConfFile()
+        {
+
         }
     }
 }
